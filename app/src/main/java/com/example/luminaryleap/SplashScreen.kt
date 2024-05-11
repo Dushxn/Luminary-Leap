7c7b37c7142fe466e0b0cc695b430b8af9f5d2d7
Tharindu Dushan <tdmahindarathne@gmail.com>
2024-05-12 00:06:13 +0530

    Added splash screen intent

--- app/src/main/java/com/example/luminaryleap/SplashScreen.kt
+++ app/src/main/java/com/example/luminaryleap/SplashScreen.kt
@@ -1,16 +1,24 @@
 package com.example.luminaryleap
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash_screen)
+
+        val i = Intent(this@SplashScreen, MainActivity::class.java)
+        Handler().postDelayed({
+            startActivity(i)
+            finish()
+        }, 2000)
 
     }
 }
